eef31ee97e354baa0d4de171258321c1d1702848
gokul101927 <gokullogan10@gmail.com>
2023-08-27 16:34:51 +0530

    Redirecting 404 to home page

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<Home />} />
           </Route>
         </Routes>
         <Footer/>
