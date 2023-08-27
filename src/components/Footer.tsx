e9eb5755ddaa7380055ad2aa0f8461e6ae9c400b
gokul101927 <gokullogan10@gmail.com>
2023-08-27 16:27:14 +0530

    Added proper links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,10 @@
-import { useNavigate } from "react-router-dom"
-
 const Footer = () => {
-    const navigate = useNavigate();
+
     return (
         <div className='container mx-auto px-4 md:px-24 py-8 flex items-end justify-end gap-8 '>
-            <img src='https://ik.imagekit.io/dpkmzcpsk/Portfolio/linkedin.png' onClick={() => navigate("https://www.linkedin.com/in/iamgokull/")} className='hover:scale-105 h-6 w-6 opacity-50 hover:opacity-100 cursor-pointer'></img>
-            <img src='https://ik.imagekit.io/dpkmzcpsk/Portfolio/github.png' onClick={() => navigate("https://github.com/iamgokull10")} className='hover:scale-105 h-6 w-6 opacity-50 hover:opacity-100 cursor-pointer'></img>
-            <img src='https://ik.imagekit.io/dpkmzcpsk/Portfolio/gmail.png' onClick={() => navigate("mailto:iamgokull10@gmail.com")} className='hover:scale-105 h-6 w-6 opacity-50 hover:opacity-100 cursor-pointer'></img>
+            <a href="https://www.linkedin.com/in/iamgokull/"><img src='https://ik.imagekit.io/dpkmzcpsk/Portfolio/linkedin.png' className='hover:scale-105 h-6 w-6 opacity-50 hover:opacity-100 cursor-pointer'></img></a>
+            <a href="https://github.com/iamgokull10"><img src='https://ik.imagekit.io/dpkmzcpsk/Portfolio/github.png' className='hover:scale-105 h-6 w-6 opacity-50 hover:opacity-100 cursor-pointer'></img></a>
+            <a href="mailto:iamgokull10@gmail.com"><img src='https://ik.imagekit.io/dpkmzcpsk/Portfolio/gmail.png' className='hover:scale-105 h-6 w-6 opacity-50 hover:opacity-100 cursor-pointer'></img></a>
         </div>
     )
 }
