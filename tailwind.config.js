8d9b02e0f43cea77146793b4a2d543322b2e1e58
gokul101927 <gokullogan10@gmail.com>
2023-08-30 09:21:29 +0530

    Refactored animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,76 +22,9 @@ export default {
         textColor: 'rgb(198, 198, 198)'
       },
       animation: {
-        fadeIn: 'fadeIn 3s forwards',
-        jumpFromTop: 'jumpFromTop 1s forwards',
-        jumpFromBottom: 'jumpFromBottom 2s forwards',
-        out: 'out 5s ease-in-out',
         jumpInfinite: 'jumpInfinite 1.5s infinite',
-        shine: 'shine 1s',
-        meteorEffect: "meteor 5s linear infinite",
       },
       keyframes: {
-        meteor: {
-          "0%": { transform: "rotate(215deg) translateX(0)", opacity: 1 },
-          "70%": { opacity: 1 },
-          "100%": {
-            transform: "rotate(215deg) translateX(-1100px)",
-            opacity: 0,
-          },
-        },
-        fadeIn: {
-          '0%': {
-            transform: 'translateX(0)',
-            opacity: '0',
-          },
-          '50%': {
-            transform: 'translateX(-50px);',
-            opacity: '0'
-          },
-          '100%': {
-            transform: 'translateX(0);',
-            opacity: '1'
-          }
-        },
-        jumpFromTop: {
-          '0%': {
-            transform: 'translateY(0)',
-            opacity: '0',
-          },
-          '50%': {
-            transform: 'translateY(-50%);',
-            opacity: '0',
-          },
-          '100%': {
-            transform: 'translateY(0);',
-            opacity: '1'
-          }
-        },
-        jumpFromBottom: {
-          '0%': {
-            transform: 'translateY(0)',
-            opacity: '0',
-          },
-          '30%': {
-            transform: 'translateY(80%);',
-            opacity: '0'
-          },
-          '100%': {
-            transform: 'translateY(0);',
-            opacity: '1'
-          }
-        },
-        out: {
-          '0%': {
-            opacity: '0',
-          },
-          '50%': {
-            opacity: '0'
-          },
-          '100%': {
-            opacity: '1'
-          }
-        },
         jumpInfinite: {
           '0%': {
             marginLeft: '0',
@@ -102,9 +35,6 @@ export default {
           '100%': {
             marginLeft: '0'
           }
-        },
-        shine: {
-          "100%": { left: "125%" },
         },
       },
     },
